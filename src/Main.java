2c315bfb020c1a8395e62c744aecba61de7e42e6
AnastasiaSergeeva05 <gotsenko_nasty@mail.ru>
2022-09-09 16:03:23 +0300

    Сделано ДЖ Переменные часть 2 ЗД№1

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        // Задание 1
+
+            int a = 1;
+            System.out.println(a);
+            byte b = 2;
+            System.out.println(b);
+            short c = 3;
+            System.out.println(c);
+            long d = 4;
+            System.out.println(d);
+            float e =5.5F;
+            System.out.println(e);
+            double f =6.5;
+            System.out.println(f);
+        }
+
+    }
